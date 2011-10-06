edafaed1a4be9a74f2bf4c287f6345bf02de4ace
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-10-06 10:46:07 +0200

    [NEW] Ajout script de migration utilisé par le module updater

---
+++ migrate.php
@@ -0,0 +1,611 @@
+<?php
+class c_ChangeMigrationScript
+{
+	static $fromRelease = '3.5.1';
+	
+	static $toRelease = '3.5.2';
+	
+	static $patchs = array(
+		//	"migrateChangeXml",
+	
+		//	"updateChangeProperties", 
+	
+		//	"cleanBuildAndCache", 
+	
+		//	"buildProject", 
+
+		//	"forums 0351", // Patch exemple
+
+		"filalizeMigration"
+		);
+		
+	public function migrateChangeXml()
+	{
+		$this->loadChangeXML();
+		$this->changeXML->save(WEBEDIT_HOME . '/change.' .self::$fromRelease .'.xml');
+		$targets = $this->getReleaseModules(self::$toRelease);
+		
+		$nodes = $this->changeXML->getElementsByTagName('framework');
+		$node = $nodes->item(0);
+		$t = $targets['framework'];
+		$this->log('framework ' .$node->textContent . ' -> ' . implode('-', $t) . PHP_EOL);
+		
+		while ($node->hasChildNodes()){$node->removeChild($node->lastChild);}
+		$node->appendChild($this->changeXML->createTextNode($t[0]));
+		if (count($t) == 2)
+		{
+			$node->setAttribute('hotfixes', $t[1]);
+		}
+		elseif ($node->hasAttribute('hotfixes'))
+		{
+			$node->removeAttribute('hotfixes');
+		}
+		
+		foreach ($this->changeXML->getElementsByTagName('module') as $node)
+		{
+			$parts = explode('-', $node->textContent);
+			if (count($parts) == 2 && isset($targets[$parts[0]]))
+			{
+				$t = $targets[$parts[0]];
+				$this->log($parts[0] . ' ' . $parts[1] . ' -> ' . implode('-', $t) . PHP_EOL);
+				while ($node->hasChildNodes()) {$node->removeChild($node->lastChild);}
+				$node->appendChild($this->changeXML->createTextNode($parts[0] . '-' . $t[0]));
+				if (count($t) == 2)
+				{
+					$node->setAttribute('hotfixes', $t[1]);
+				}
+				elseif ($node->hasAttribute('hotfixes'))
+				{
+					$node->removeAttribute('hotfixes');
+				}
+			}
+		}
+			
+		$this->changeXML->save(WEBEDIT_HOME . '/change.xml');
+	}
+	
+	public function updateChangeProperties()
+	{
+		//$this->updateProjectProperties('DEVELOPMENT_MODE', 'true');
+		//$this->saveProjectProperties();
+	}
+		
+	public function cleanBuildAndCache()
+	{
+			
+		@unlink(WEBEDIT_HOME . "/.computedChangeComponents.ser");
+
+		$this->rmdir(WEBEDIT_HOME . "/build");
+		$this->rmdir(WEBEDIT_HOME . "/cache/" . $this->getProfile());
+		$this->rmdir(WEBEDIT_HOME . "/cache/www");
+	}
+	
+	public function buildProject()
+	{
+		$this->executeTask("init-project");
+		
+		$this->executeTask("init-webapp");
+		
+		$this->executeTask("compile-config");
+		
+		$this->executeTask("compile-documents");
+		
+		$this->executeTask("generate-database");
+		
+		$this->executeTask("compile-db-schema");
+	}
+	
+	public function filalizeMigration()
+	{
+		$this->executeTask("clear-all");
+		
+		$this->executeTask("compile-all");
+	}
+	
+	
+	//TOOL FUNCTIONS
+
+	/**
+	 * @var array
+	 */
+	protected $projectProperties;
+	
+	/**
+	 * @var string
+	 */
+	protected $profile;
+	
+	/**
+	 * @var DOMDocument
+	 */
+	protected $changeXML;
+	
+	/**
+	 * @param string $module
+	 * @param string $number
+	 */
+	protected function applyPatch($module, $number)
+	{
+		if ($module === 'framework' || file_exists(WEBEDIT_HOME . '/modules/' . $module . '/patch/' . $number . '/install.php'))
+		{
+			$this->executeTask('apply-patch', array($module, $number));
+		}
+		else
+		{
+			$this->log("Patch not found ". $module. " ". $number. PHP_EOL, 'warn');
+		}
+	}	
+	
+	/**
+	 * @param String $task
+	 * @param String[] $args
+	 * @return string
+	 */
+	protected function executeTask($task, $args = array(), $log = true)
+	{
+		$phpCli = $this->getProjectProperty('PHP_CLI_PATH');
+		if ($phpCli === null) {$phpCli = "php";}
+		if (!empty($phpCli)) {$phpCli .= ' ';}
+		
+		$cmd = $phpCli . WEBEDIT_HOME . "/framework/bin/change.php $task " . implode(" ", $args);
+		$result = $this->exec($cmd);
+		if ($log) {$this->log($result);}
+		return $result;
+	}
+	
+	/**
+	 * @param string $cmd
+	 */
+	private function exec($cmd)
+	{
+		$descriptorspec = array(
+			0 => array('pipe', 'r'), // stdin
+			1 => array('pipe', 'w'), // stdout
+			2 => array('pipe', 'w') // stderr
+		);
+		
+		$proc = proc_open($cmd, $descriptorspec, $pipes, WEBEDIT_HOME);
+		if (!is_resource($proc))
+		{
+			$this->log("Can not execute $cmd" . PHP_EOL, 'error');
+			return null;
+		}
+		
+		$output = array();
+		
+		stream_set_blocking($pipes[2], 0);
+		fclose($pipes[0]);		
+		while (!feof($pipes[1]))
+		{
+			$s = fread($pipes[1], 512);
+			if ($s === false)
+			{
+				$this->log("Error while executing $cmd: could not read further execution result" . PHP_EOL, 'error');
+				break;
+			}
+			$output[] = $s;
+		}
+
+		$retVal = proc_close($proc);
+		if (0 != $retVal)
+		{
+			$this->log("Could not execute $cmd (exit code $retVal)" . PHP_EOL, 'error');
+		}
+		return implode('', $output);
+	}
+	
+	/**
+	 * @param string $directoryPath
+	 * @param boolean $onlyContent
+	 */
+	protected function rmdir($directoryPath, $onlyContent = false)
+	{
+		if (is_dir($directoryPath))
+		{
+			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directoryPath, RecursiveDirectoryIterator::KEY_AS_PATHNAME), RecursiveIteratorIterator::CHILD_FIRST) as $file => $info)
+			{
+				$fileName = $info->getFilename();
+				if ($fileName === '.' || $fileName === '..')
+				{
+					continue;
+				}
+				if ($info->isFile() || $this->isLink($info->getPathname()))
+				{
+					unlink($file);
+				}
+				else if ($info->isDir())
+				{
+					rmdir($file);
+				}
+			}
+			if (!$onlyContent)
+			{
+				rmdir($directoryPath);
+			}
+		}
+	}
+	
+	/**
+	 * @param string $filepath
+	 * @return boolean
+	 */
+	protected function isLink($filepath)
+	{
+		if (DIRECTORY_SEPARATOR === '/')
+		{
+			return is_link($filepath);
+		}
+		
+		if (file_exists($filepath) && self::normalizePath($filepath) != readlink($filepath))
+		{
+			return true;
+		}
+		return false;
+	}	
+	
+	/**
+	 * @param string $url
+	 * @param string $curldata
+	 * @param string || false $err
+	 */
+	protected function wget($url, &$curldata, &$err)
+	{
+		$cr = curl_init();
+		$options = array(CURLOPT_RETURNTRANSFER => true, 
+			CURLOPT_TIMEOUT => 60, CURLOPT_CONNECTTIMEOUT => 5,
+		 	CURLOPT_FOLLOWLOCATION => false, CURLOPT_URL => $url, CURLOPT_POSTFIELDS => null, 
+		 	CURLOPT_POST => false, CURLOPT_SSL_VERIFYPEER =>false);
+		curl_setopt_array( $cr, $options );
+		$curldata = curl_exec( $cr );
+		$errNumber = curl_errno( $cr );
+		if ($errNumber !== 0)
+		{
+			$err = curl_error($cr) . ' (code: ' . $errNumber . ')';
+		}
+		else
+		{
+			$err = false;
+		}
+		curl_close( $cr );
+	}
+	
+	/**
+	 * @return String
+	 */
+	protected function getProfile()
+	{
+		return trim(file_get_contents(WEBEDIT_HOME . "/profile"));
+	}
+	
+	/**
+	 * @return boolean
+	 */
+	protected function loadChangeXML()
+	{
+		$this->changeXML = new DOMDocument('1.0', 'UTF-8');
+		if (!$this->changeXML->load(WEBEDIT_HOME . '/change.xml'))
+		{
+			$this->log('Unable to load :' . WEBEDIT_HOME . '/change.xml' . PHP_EOL, 'error');
+			$this->changeXML = false;
+			return false;
+		}
+		
+		$framework = $this->changeXML->getElementsByTagName('framework');
+		if ($framework->length != 1)
+		{
+			$this->log('Unable to read framework version.' . PHP_EOL, 'error');
+			$this->changeXML = false;
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * @return string;
+	 */
+	protected function getCurrentRelease()
+	{
+		if ($this->changeXML === null) {$this->loadChangeXML();}
+		
+		if ($this->changeXML)
+		{
+			return $this->changeXML->getElementsByTagName('framework')->item(0)->textContent;
+		}
+		return null;
+	}
+	
+	/**
+	 * @param string $moduleName
+	 * @return boolean
+	 */
+	protected function moduleExist($moduleName)
+	{
+		return file_exists(WEBEDIT_HOME . '/modules/' . $moduleName);
+	}
+	
+	/**
+	 * @param string $moduleName
+	 * @param string $version
+	 * @return boolean
+	 */
+	protected function checkModuleVersion($moduleName, $version = null)
+	{
+		if ($version === null) {$version = self::$toRelease;}
+		
+		if ($this->changeXML === null) {$this->loadChangeXML();}
+		if ($this->changeXML)
+		{
+			$modules = $this->changeXML->getElementsByTagName('module');
+			foreach ($modules as $module)
+			{
+				if ($module->textContent == $moduleName . '-' . $version)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return boolean
+	 */
+	protected function loadProjectProperties()
+	{
+		$changePropertiesPath = WEBEDIT_HOME . '/change.properties';
+		if (is_readable($changePropertiesPath))
+		{
+			$this->projectProperties = file($changePropertiesPath);	
+			return true;
+		}
+		$this->log('Unable to load : ' . $changePropertiesPath . PHP_EOL, 'error');
+		$this->projectProperties = array();
+		return false;
+	}
+	
+	/**
+	 * @param string $name
+	 * @param string $value
+	 */
+	protected function updateProjectProperties($name, $value)
+	{
+		foreach ($this->projectProperties as $index => $line)
+		{
+			if (strpos($line, $name) === 0)
+			{
+				$this->projectProperties[$index] = $name . '=' . $value . PHP_EOL;
+				return;
+			}
+		}
+		$this->projectProperties[] = PHP_EOL . $name . '=' . $value . PHP_EOL;
+	}
+	
+	protected function getProjectProperty($name)
+	{
+		foreach ($this->projectProperties as $index => $line)
+		{
+			if (strpos($line, $name) === 0)
+			{
+				$lineParts = explode("=", $line);
+				if (count($lineParts) > 1)
+				{
+					$valueParts = explode("#", $lineParts[1]);
+					return trim($valueParts[0]);
+				}
+				return null;
+			}
+		}
+		return null;
+	}
+	
+	protected function saveProjectProperties()
+	{
+		file_put_contents(WEBEDIT_HOME . '/change.properties', 
+				implode('', $this->projectProperties));
+	}
+	
+	
+	/**
+	 * @param string $releaseVersion
+	 * @return array
+	 */
+	protected function getReleaseModules($releaseVersion)
+	{
+		$result = array();
+		$filename = WEBEDIT_HOME . '/migration/release-' . $releaseVersion . '.xml';
+		$doc = new DOMDocument('1.0', 'UTF-8');
+		$doc->load($filename);
+		$xpath = new DOMXPath($doc);
+		
+		foreach ($xpath->query('//change-lib[@name="framework"]') as $node) 
+		{
+			$result['framework'] = array($node->getAttribute('version'));
+			$hotFixes = $xpath->query('hotfix', $node);
+			if ($hotFixes->length > 0)
+			{
+				$result['framework'][] = $hotFixes->item($hotFixes->length - 1)->getAttribute('number');
+			}
+		}
+		
+		foreach ($xpath->query('//module') as $node) 
+		{
+			$result[$node->getAttribute('name')] = array($node->getAttribute('version'));
+			$hotFixes = $xpath->query('hotfix', $node);
+			if ($hotFixes->length > 0)
+			{
+				$result[$node->getAttribute('name')][] = $hotFixes->item($hotFixes->length - 1)->getAttribute('number');
+			}
+		}
+		return $result;
+	}	
+	
+	
+	public function main()
+	{
+		$this->profile = $this->getProfile();	
+		$this->loadProjectProperties();	
+		foreach (self::$patchs as $patch)
+		{
+			if (strpos($patch, ' '))
+			{
+				list ($module, $number) = explode(' ', $patch);
+				$this->applyPatch($module, $number);
+			}
+			else
+			{
+				$this->{$patch}();
+			}
+		}
+	}
+	
+	public function executeStep($stepIndex)
+	{
+		if (isset(self::$patchs[$stepIndex]))
+		{
+			$this->profile = $this->getProfile();
+			$this->loadProjectProperties();	
+						
+			$patch = self::$patchs[$stepIndex];			
+			if (strpos($patch, ' '))
+			{
+				list ($module, $number) = explode(' ', $patch);
+				$this->applyPatch($module, $number);
+			}
+			else
+			{
+				$this->{$patch}();
+			}
+			return true;
+		}
+		else
+		{
+			$this->log('Step '. $stepIndex. ' not found' . PHP_EOL, 'error');
+		}
+		return false;
+	}
+	
+	public function check()
+	{		
+		$profilePath = WEBEDIT_HOME . "/profile";
+		if (!file_exists($profilePath))
+		{
+			$this->log('Profile file not found'. PHP_EOL, 'error');
+			return false;
+		}
+		$this->profile = $this->getProfile();
+		
+		if (!$this->loadChangeXML())
+		{
+			return false;
+		}
+
+		if ($this->getCurrentRelease() != self::$fromRelease)
+		{
+			$this->log('Invalid current release: ' . $this->getCurrentRelease() . PHP_EOL, 'error');
+			return false;
+		}
+
+		if (!$this->loadProjectProperties())
+		{
+			return false;
+		}
+			
+		if ($this->checkRelease())
+		{
+			if ($this->checkExecution())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	protected function checkRelease()
+	{
+		$string = $this->getProjectProperty("REMOTE_REPOSITORIES");
+		if (empty($string)) {$string = "http://osrepo.rbschange.fr";}		
+		$remoteRepositories = array();
+		foreach (explode(",", $string) as $repoUrl) 
+		{
+			$repoUrl = trim($repoUrl);
+			if (!empty($repoUrl)) {$remoteRepositories[] = $repoUrl;}
+		}
+		$fromName = 'release-' . self::$fromRelease . '.xml';
+		$doc = $this->getRelaseDocument($remoteRepositories, $fromName);
+		if ($doc)
+		{
+			$doc->save(WEBEDIT_HOME . '/migration/' . $fromName);
+		}
+		else
+		{
+			$this->log('Unable to download ' . $fromName, 'error');
+			return false;
+		}
+		
+		$toName = 'release-' . self::$toRelease . '.xml';
+		$doc = $this->getRelaseDocument($remoteRepositories, $toName);
+		if ($doc)
+		{
+			$doc->save(WEBEDIT_HOME . '/migration/' . $toName);
+		}
+		else
+		{
+			$this->log('Unable to download ' . $toName, 'error');
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param string[] $repositories
+	 * @param string $name
+	 * @return DOMDocument || null
+	 */
+	protected function getRelaseDocument($repositories, $name)
+	{
+		foreach ($repositories as $repoUrl) 
+		{
+			$url = $repoUrl . '/' . $name;
+			$data = ''; $errno = false;
+			$this->wget($url, $data, $errno);
+			if ($errno === false && !empty($data))
+			{
+				$doc = new DOMDocument('1.0', 'UTF-8');
+				if ($doc->loadXML($data))
+				{
+					return $doc;
+				}
+			}
+		}
+		return null;
+	}
+	
+	protected function checkExecution()
+	{
+		$result = $this->executeTask("updater.migrate", array('--check'));
+		return (strpos($result, 'CHECK SUCCESS') !== false);
+	}
+	
+	public function log($message, $level = "info")
+	{
+		if ($level === 'warn' || $level === 'error')
+		{
+			echo strtoupper($level) , ': ', $message;
+		}
+		else
+		{
+			echo $message;
+		}
+	}
+}
+
+if (!defined('WEBEDIT_HOME'))
+{
+	define("WEBEDIT_HOME", getcwd());
+	$migration = new c_ChangeMigrationScript();
+	if ($migration->check())
+	{
+		$migration->main();
+	}
+}
---
+++ migrateweb.php
@@ -0,0 +1,130 @@
+<?php
+header('Last-Modified' .': '. gmdate('D, d M Y H:i:s') . ' GMT');
+header('Expires' .': '. 'Mon, 26 Jul 1997 05:00:00 GMT');
+header('Cache-Control' .': '. 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Pragma' .': '. 'no-cache');
+ignore_user_abort(true);
+set_time_limit(0);
+umask(0002);
+session_start();
+
+define("WEBEDIT_HOME", dirname(dirname(realpath(__FILE__))));
+chdir(WEBEDIT_HOME);
+
+require_once WEBEDIT_HOME . '/migration/migrate.php';
+
+class c_ChangeMigrationHTTPScript extends c_ChangeMigrationScript
+{
+	public function executeTask($task, $args = array(), $log = true)
+	{
+		$https = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "off";
+		$srcUrl = "http".(($https) ? "s" : "")."://".$_SERVER["HTTP_HOST"]."/migration/migrateweb.php";
+		$params = http_build_query(array('cmd' => $task . ' ' . implode(' ', $args)));
+		$curldata = null;
+		$errno = 0;
+		$this->wget($srcUrl . '?' . $params, $curldata, $errno);
+		if ($errno !== false)
+		{
+			$this->log($errno, 'error');
+		}
+		if ($log) {echo $curldata;}
+		return $curldata;
+	}
+	
+	function log($message, $level = 'info')
+	{
+		echo 'MSGTYPE:', $level, ':', $message;
+	}
+	
+	function convertLogToArray($string)
+	{
+		$lines = array();
+		if (empty($string)) {return $string;}
+		
+		foreach (explode(PHP_EOL, $string) as $line) 
+		{
+			if (empty($line)) {continue;}
+			if (preg_match('/^MSGTYPE:([a-z]+):/', $line, $match))
+			{
+				switch ($match[1])
+				{
+					case 'warn':
+						$lines[] = array('warn', substr($line, strlen($match[0])));
+						break;
+					case 'error':
+						$lines[] = array('error', substr($line, strlen($match[0])));
+						break;
+					default:
+						$lines[] = array('info', substr($line, strlen($match[0])));
+				}
+			}
+			else
+			{
+				$lines[] = array('info', $line);
+			}
+		}
+		return $lines;
+	}
+}
+
+if (isset($_GET['execStep']))
+{
+	header('Content-Type: application/json; charset=utf-8');
+	
+	ob_start();
+	$migration = new c_ChangeMigrationHTTPScript();
+	$migration->executeStep($_GET['execStep']);
+	$string = ob_get_clean();
+	$lines = $migration->convertLogToArray($string);
+	echo json_encode(array('logs' => $lines));
+}
+elseif (isset($_GET['check']))
+{
+	header('Content-Type: application/json; charset=utf-8');
+	ob_start();
+	$migration = new c_ChangeMigrationHTTPScript();
+	$checked = $migration->check();
+	$string = ob_get_clean();
+	$lines = $migration->convertLogToArray($string);
+	echo json_encode(array('logs' => $lines, 'checked' => $checked));
+}
+elseif (isset($_GET['cmd']))
+{
+	
+	$profile = @file_get_contents(WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'profile');
+	define('PROFILE', trim($profile));
+	define('FRAMEWORK_HOME', WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'framework');
+	define('AG_CACHE_DIR', WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . PROFILE);
+		
+	require_once FRAMEWORK_HOME . '/bin/bootstrap.php';
+	$bootStrap = new c_ChangeBootStrap(WEBEDIT_HOME);
+	$bootStrap->setAutoloadPath(WEBEDIT_HOME."/cache/autoload");
+	$argv = explode(' ', $_GET['cmd']);
+	
+	class c_ChangeBootStrapScript extends c_Changescript
+	{
+		protected function echoMessage($message, $color = null)
+		{
+			if ($color == 31)
+			{
+				echo 'MSGTYPE:error:' , $message;
+			}
+			elseif ($color == 35)
+			{
+				echo 'MSGTYPE:warn:' , $message;
+			}
+			else
+			{
+				echo $message;
+			}
+		}
+	}
+
+	$script = new c_ChangeBootStrapScript(__FILE__, FRAMEWORK_HOME, 'change');
+	require_once FRAMEWORK_HOME . '/bin/change_script.inc';
+}
+else
+{
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode(array('steps' => c_ChangeMigrationScript::$patchs));
+}
