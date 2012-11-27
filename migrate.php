d7be9e32a66338031b73fb39bfb25182637d05fe
Renaud JENNY <renaud.jenny@rbs.fr>
2012-11-27 11:03:37 +0100

    Prepare migration 3.5.7 -> 3.5.8

--- migrate.php
+++ migrate.php
@@ -3,9 +3,9 @@ class c_ChangeMigrationScript
 {
 	const REMOTE_REPOSITORY = 'http://update.rbschange.fr';
 	
-	static $fromRelease = '3.5.6';
+	static $fromRelease = '3.5.7';
 	
-	static $toRelease = '3.5.7';
+	static $toRelease = '3.5.8';
 	
 	static $patchs = array(
 		"lockApache",
@@ -24,12 +24,8 @@ class c_ChangeMigrationScript
 		
 		"buildProject",
 		
-		//No Patch found Ex "framework 0350"
+		"catalog 0364", //Absence de la liste modules_catalog/shippingmodeoptions	
 	
-		"catalog 0363", // Import new list modules_catalog/availablelangsforshop.
-		
-		"joboffer 0350", // Update spontaneous offer.
-		
 		"clearAll",
 		
 		"compileAll",
